42759a3c46e7816fa403a31fdf8b72842a987563
HannaWang003 <hannawang003@gmail.com>
2024-06-26 17:19:21 +0800

    feat:輪播圖完成

--- front/main.php
+++ front/main.php
@@ -1,74 +1,73 @@
 <style>
-.lists {
-    width: 200px;
-    height: 240px;
-    position: relative;
-    left: 114px;
-    overflow: hidden;
-}
+    .lists {
+        width: 200px;
+        height: 240px;
+        overflow: hidden;
+        margin: auto;
+    }
 
-.item {
-    box-sizing: border-box;
-    width: 200px;
-    height: 240px;
-    margin: auto;
-    position: absolute;
-    display: none;
-    text-align: center;
-}
+    .item {
+        box-sizing: border-box;
+        width: 200px;
+        height: 240px;
+        margin: auto;
+        position: absolute;
+        text-align: center;
+        display: none;
+    }
 
-.item div img {
-    width: 100%;
-    height: 220px;
-}
+    .item div img {
+        width: 100%;
+        height: 220px;
+    }
 
-/* 按鈕 */
-.controls {
-    width: 420px;
-    height: 100px;
-    margin: auto;
-    margin-top: 10px;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-}
+    /* 按鈕 */
+    .controls {
+        width: 420px;
+        height: 100px;
+        margin: auto;
+        margin-top: 10px;
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
 
-.left,
-.right {
-    width: 0;
-    border: 20px solid black;
-    border-top-color: transparent;
-    border-bottom-color: transparent;
-}
+    .left,
+    .right {
+        width: 0;
+        border: 20px solid black;
+        border-top-color: transparent;
+        border-bottom-color: transparent;
+    }
 
-.left {
-    border-left-width: 0;
-}
+    .left {
+        border-left-width: 0;
+    }
 
-.right {
-    border-right-width: 0;
-}
+    .right {
+        border-right-width: 0;
+    }
 
-.btns {
-    width: 360px;
-    height: 100px;
-    display: flex;
-    overflow: hidden;
-}
+    .btns {
+        width: 360px;
+        height: 100px;
+        display: flex;
+        overflow: hidden;
+    }
 
-.btn {
-    width: 90px;
-    flex-shrink: 0;
-    text-align: center;
-    font-size: 12px;
-    position: relative;
-}
+    .btn {
+        width: 90px;
+        flex-shrink: 0;
+        text-align: center;
+        font-size: 12px;
+        position: relative;
+    }
 
-.btn img {
-    width: 60px;
-    height: 80px;
-}
+    .btn img {
+        width: 60px;
+        height: 80px;
+    }
 </style>
 <?php
 $posters = $Poster->all(['sh' => 1], "order by rank");
@@ -82,10 +81,10 @@ $posters = $Poster->all(['sh' => 1], "order by rank");
             <?php
             foreach ($posters as $idx => $poster) {
             ?>
-            <div class="item">
-                <div><img src="./img/<?= $poster['img'] ?>" alt=""></div>
-                <div><?=$poster['name']?></div>
-            </div>
+                <div class="item" data-ani="<?= $poster['ani'] ?>">
+                    <div><img src="./img/<?= $poster['img'] ?>" alt=""></div>
+                    <div><?= $poster['name'] ?></div>
+                </div>
             <?php
             }
             ?>
@@ -100,10 +99,10 @@ $posters = $Poster->all(['sh' => 1], "order by rank");
                 <?php
                 foreach ($posters as $idx => $poster) {
                 ?>
-                <div class="btn">
-                    <div><img src="./img/<?= $poster['img'] ?>" alt=""></div>
-                    <div><?=$poster['name']?></div>
-                </div>
+                    <div class="btn">
+                        <div><img src="./img/<?= $poster['img'] ?>" alt=""></div>
+                        <div><?= $poster['name'] ?></div>
+                    </div>
                 <?php
                 }
                 ?>
@@ -124,3 +123,39 @@ $posters = $Poster->all(['sh' => 1], "order by rank");
         <div class="ct"> </div>
     </div>
 </div>
+<script>
+    let now = 0;
+    let next = 0;
+    let total = $('.item').length;
+    let timer = setInterval(slide, 3000);
+
+    function slide(n) {
+        let ani = $('.item').eq(now).data('ani');
+        if (typeof(n) == "undefined") {
+            next = now + 1;
+            if (next >= total) {
+                next = 0
+            }
+        } else {
+            next = n;
+        }
+        switch (ani) {
+            case 1:
+                $('.item').eq(now).fadeOut(1000, () => {
+                    $('.item').eq(next).fadeIn(1000);
+                })
+                break;
+            case 2:
+                $('.item').eq(now).slideUp(1000, () => {
+                    $('.item').eq(next).slideDown(1000);
+                })
+                break;
+            case 3:
+                $('.item').eq(now).hide(1000, () => {
+                    $('.item').eq(next).show(1000);
+                })
+                break;
+        }
+        now = next;
+    }
+</script>
